63c23008cabd9f8e211e686d21faa1476bfa7672
RamiroVT <u202123714@upc.edu.pe>
2024-04-28 22:11:24 -0500

    add: eureka config

--- src/main/java/org/Arquitech/Gymrat/classservice/ClassServiceApplication.java
+++ src/main/java/org/Arquitech/Gymrat/classservice/ClassServiceApplication.java
@@ -1,8 +1,19 @@
 package org.Arquitech.Gymrat.classservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
+@OpenAPIDefinition(
+		info = @Info(
+				title = "API Class - GymRat",
+				version = "1.0",
+				description = "API to manage classes part of the application called GymRat"
+		)
+)
 @SpringBootApplication
 public class ClassServiceApplication {
 
