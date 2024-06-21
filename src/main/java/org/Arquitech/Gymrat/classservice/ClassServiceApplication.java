0f93f08ecb1484b468d4f1a0fdaa5d84c71df8b8
RamiroVT <u202123714@upc.edu.pe>
2024-06-20 19:11:09 -0500

    feat: add env config

--- src/main/java/org/Arquitech/Gymrat/classservice/ClassServiceApplication.java
+++ src/main/java/org/Arquitech/Gymrat/classservice/ClassServiceApplication.java
@@ -1,10 +1,10 @@
 package org.Arquitech.Gymrat.classservice;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 
 
 @OpenAPIDefinition(
@@ -18,7 +18,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ClassServiceApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("RAILWAY_DB_HOST", dotenv.get("RAILWAY_DB_HOST"));
+		System.setProperty("RAILWAY_DB_PORT", dotenv.get("RAILWAY_DB_PORT"));
+		System.setProperty("RAILWAY_DB_USERNAME", dotenv.get("RAILWAY_DB_USERNAME"));
+		System.setProperty("RAILWAY_DB_PASSWORD", dotenv.get("RAILWAY_DB_PASSWORD"));
+		System.setProperty("RAILWAY_DB_NAME", dotenv.get("RAILWAY_DB_NAME"));
+
 		SpringApplication.run(ClassServiceApplication.class, args);
 	}
-
 }
